53ceaf4de1c8a9a7088283210e1d8082c321e6e6
donovan <donovan.fremy@gmail.com>
2018-07-19 16:35:21 +0200

    add bd domain

--- src/main/java/client/domain/Bd.java
+++ src/main/java/client/domain/Bd.java
@@ -1,5 +1,7 @@
 package client.domain;
 
+import client.wrapper.BdAuthor;
+import client.wrapper.BdName;
 import framework.domain.Entity;
 
 import javax.persistence.Column;
@@ -7,5 +9,9 @@ import javax.persistence.Table;
 
 @Table
 public class Bd extends Entity<Bd> {
+    @Column
+    private BdName BdName;
 
+    @Column
+    private BdAuthor BdAuthor;
 }
